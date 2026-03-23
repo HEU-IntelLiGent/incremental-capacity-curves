5a6304cd7ddfdf7b7b2193994c993c77529da71e
Eibar Joel Flores Cedeño <eibar.flores.cedeno@sintef.no>
2026-03-23 13:18:36 +0100

    removed unnnecesary functions to compute dqdvs

--- dqdvs.py
+++ dqdvs.py
@@ -31,6 +31,7 @@ def dqdv_finite_differences(q:np.ndarray, v:np.ndarray)->np.ndarray:
     return v[mask], dqdv[mask] 
 
 
+
 def dqdv_central_differences(q:np.ndarray, v:np.ndarray)->tuple[np.ndarray, np.ndarray]:
     """
     Compute the numerical first differences dq/dv.
@@ -58,99 +59,6 @@ def dqdv_central_differences(q:np.ndarray, v:np.ndarray)->tuple[np.ndarray, np.n
 
 
 
-def bin_array(x:np.ndarray, bin_size:float)->np.ndarray:
-    """
-    Bin the input array x to specified intervals.
-
-    Rounds values to the nearest multiple of bin_size and ensures 
-    the output is either monotonically increasing or decreasing 
-    based on the increasing flag.
-
-    Parameters:
-    -----------
-    x : np.ndarray
-        Input array to be binned.
-    
-    bin_size : float
-        Size of the bins.
-
-    increasing : bool
-        If True, output is monotonically increasing; if False, decreasing.
-
-    Returns:
-    --------
-    np.ndarray
-        Binned array.
-    """
-
-    x_bin = (x / bin_size).round() * bin_size #round to intervals of size bin_size
-    increasing = bool(x[-1] > x[0])
-
-    if increasing:
-        return np.maximum.accumulate(x_bin) #ensure array is monotonically increasing
-    else:
-        return np.minimum.accumulate(x_bin)
-
-
-
-
-def remove_duplicate_voltages(q: np.ndarray, v: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Remove duplicate voltage values from the arrays q and v.
-
-    Flags adjacent duplicate voltage entries in the monotonic voltage array 
-    and retains only unique values.
-
-    Parameters:
-    -----------
-    q : np.ndarray
-        Cumulative capacity data corresponding to the voltages.
-    
-    v : np.ndarray
-        Monotonic voltage data.
-
-    Returns:
-    --------
-    tuple[np.ndarray, np.ndarray]
-        Two arrays with duplicates removed: (q, v).
-    """
-    mask = np.concatenate((np.diff(v) != 0, [True]))  # flags duplicate voltage values
-    return q[mask], v[mask]
-
-
-
-def compute_dqdv_binning(q:np.ndarray, v:np.ndarray, v_increases:bool)->np.ndarray:
-    """
-    Compute the rate of change dq/dV based on cumulative capacity and voltage.
-
-    Bins the voltage values, removes duplicates, and calculates the 
-    numerical first differences of cumulative capacity relative to 
-    voltage.
-
-    Parameters:
-    -----------
-    q : np.ndarray
-        Cumulative capacity data.
-    
-    v : np.ndarray
-        Voltage data, which can be either increasing or decreasing.
-    
-    v_increases : bool
-        Indicates if the voltage values are increasing.
-
-    Returns:
-    --------
-    np.ndarray
-        Array of dq/dV values after processing.
-    """
-
-    bin_size = optimal_bin_size(v)
-    v_bin = bin_array(v, bin_size, v_increases)
-    q_nodup, v_nodup = remove_duplicate_voltages(q, v_bin)
-    return dydx_simple_differences(x=v_nodup, y=q_nodup)
-
-
-
 def compute_monotonic_ocv(q:np.ndarray, v:np.ndarray, epsilon:float):
     """
     Computes a monotonic OCV by 1) applying a cumulative monotonic envelope (running max for increasing series, 
@@ -175,7 +83,6 @@ def compute_monotonic_ocv(q:np.ndarray, v:np.ndarray, epsilon:float):
         idx_keep = np.where(np.diff(v_mon) < -epsilon)[0]
         
     return q[idx_keep], v_mon[idx_keep]
-
 
 
 
