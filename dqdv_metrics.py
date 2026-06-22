925d619b69976618ae62bcfa4851ac69d3f04a47
Eibar Flores <111871848+eibar-flores@users.noreply.github.com>
2026-06-22 11:17:27 +0200

    Update dqdv_metrics.py with citation notice

--- dqdv_metrics.py
+++ dqdv_metrics.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: MIT
+# License: MIT. See LICENSE file for details.
+# If you use this script, please cite: Flores et al 2026 J. Electrochem. Soc. https://doi.org/10.1149/1945-7111/ae7e5c.
+
 import numpy as np
 
 def absolute_percentage_error(original:np.ndarray, reconst:np.ndarray)->float:
